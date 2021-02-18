e9f32329807ad4af60c2465141a56392caea400d
kenzhetaeva <kenzhetaeva11@gmail.com>
2021-02-18 18:18:06 +0600

    bootstrap included into twig

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,32 +4,41 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RegistrationController extends AbstractController
 {
+    /**
+     * @Route("/register", name="app_register")
+     */
     public function registrationAction(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $em = $this->getDoctrine()->getManager();
 
         $user = new User();
 
-        $form = $this->createForm('App\Form\LoginFormType', $user);
+        $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() & $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()){
 
-            $user->setPassword($encoder->encodePassword($user, $user->getPassword()))
+            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
 
             $em->persist($user);
             $em->flush();
+
+//            return $this->redirectToRoute('login');
         }
 
-        return $this->render('others/login.html.twig', array(
-            'loginForm' => $form->createView()
+        return $this->render('security/registration.html.twig', array(
+            'signupForm' => $form->createView()
         ));
     }
 
